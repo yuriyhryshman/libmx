56ccafcc485258134cc3f893650b7d44ec0d7ed5
yuriyhryshman <82230480+yuriyhryshman@users.noreply.github.com>
2021-06-03 21:38:49 +0300

    lib

--- src/mx_itoa.c
+++ src/mx_itoa.c
@@ -1,32 +1,51 @@
 #include "header.h"
 
 char *mx_itoa(int number) {
-    int nbr = number;
+    int nbr = 0;
     int count = 0;
+    int k = 0;
+    bool i = 0;
+    
+    if (number < 0 && number != -2147483648) {
+      number = number * -1;
+      count++;
+      i = 1;
+    }
+    
+    if (number < 10) count++;
 
+    nbr = number; 
+    
     while (nbr / 10 > 0) {
         nbr /= 10;
         count++;
     }
 
-    char *string = mx_strnew(count);
+    if (i == 1) count++;
+
+    char *string = mx_strnew(count + 1);
 
     if (number == 0) return "0";
-    if (number < 0) {
-        string[0] = '-';
-        number *= -1;
+    if (number == -2147483648) return "-2147483648";
+
+    
+    for (; number != 0 && k <= count; number /= 10) {
+        string[k] = number % 10 + 48;
+        k++;
     }
 
-    for (int syka = number; syka != 0; syka /= 10) {
-        string[count] = syka % 10 + 48;
-        count--;
+    if (i == 1) {
+        string[k] = '-';
     }
-    
+
+
+    mx_str_reverse(string);
     return string;
 }
 
-
+/*
 int main() {
-    printf("%s", mx_itoa(-123));
+    printf("%s", mx_itoa(2147483647));
 }
+*/
 
