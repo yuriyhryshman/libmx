262415763d57293e69e8c317f18e0dda61f7781e
yuriyhryshman <82230480+yuriyhryshman@users.noreply.github.com>
2021-06-17 20:17:12 +0300

    Ebal ya rot

--- src/mx_memccpy.c
+++ src/mx_memccpy.c
@@ -1,24 +1,24 @@
 #include "header.h"
 
 void *mx_memccpy(void *restrict dst, const void *restrict src, int c, size_t n) {
-    const char *source = src;
-    size_t i;
+    // unsigned const char *source = src;
+    // unsigned char *dest = dst;
+    size_t i = 0;
 
-    if (n != 0) {
-        for (i = 0; i <= n; i++)
-            if (source[i] != c) return dst = mx_memcpy(dst, src, n);
-        return NULL;
-    }
+    for (; i < n; i++) {
+        ((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+        if (((const unsigned char *)src)[i] == c) return dst;
+    }     
     return NULL;
 }
 
 
-
+/*
 int main() {
     unsigned char src[25] = "0123456789ABCDEF";
-    unsigned char destination[25] = "";
-    mx_memccpy(destination, src, '4', 5);
-    printf("%s", destination);
+    unsigned char dst[25] = "";
+    mx_memccpy(dst, src, '!', 7);
+    printf("%s", dst);
 }
-
+*/
 
